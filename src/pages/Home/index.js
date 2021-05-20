8c89bbff758066ab53827db8fda570c3096882c6
pablojr17 <pablojr17@gmail.com>
2021-05-20 10:57:43 -0300

    Scroll View x FlatList

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Platform } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Platform, ScrollView, FlatList } from 'react-native';
 import { Button } from '../../components/Button';
 import { SkillCard } from '../../components/SkillCard';
 
@@ -28,13 +28,13 @@ export default function Home() {
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
 
-      {mySkills.map(skill => (
-        <SkillCard
-          key={skill}
-          skill={skill}
-        />
-      ))
-      }
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <SkillCard skill={item} />
+        )}
+      />
     </View>
   )
 }
