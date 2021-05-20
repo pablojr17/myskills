3f37ff1e77d29253773eea411187c063c1c2597c
pablojr17 <pablojr17@gmail.com>
2021-05-20 16:18:21 -0300

    status bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Platform, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Platform, FlatList } from 'react-native';
 import { Button } from '../../components/Button';
 import { SkillCard } from '../../components/SkillCard';
 
